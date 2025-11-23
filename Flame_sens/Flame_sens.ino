6e9ce97f4e241d72e4db92c09a3844e30efa162e
kareemibrahim19 <kareemrakha@std.mans.edu.eg>
2025-11-23 16:48:52 +0200

    Add files via upload

---
+++ Flame_sens/Flame_sens.ino
@@ -0,0 +1,19 @@
+int flamePin = A0;
+int buzzerPin = 8;
+
+void setup() {
+  pinMode(buzzerPin, OUTPUT);
+}
+
+void loop() {
+  int flameValue = analogRead(flamePin);
+
+  if (flameValue < 500) {
+    digitalWrite(buzzerPin, HIGH);
+  } 
+  else {
+    digitalWrite(buzzerPin, LOW);
+  }
+
+  delay(100);
+}
